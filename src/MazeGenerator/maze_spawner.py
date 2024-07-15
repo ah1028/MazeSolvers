06368f4a2b79ff0e7f39a9e598a3c41faf6514a9
Landydi0n <142226868+Landydi0n@users.noreply.github.com>
2024-07-15 16:00:15 +0100

    Added graph of maze

--- src/MazeGenerator/maze_spawner.py
+++ src/MazeGenerator/maze_spawner.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from MazeGenerator.recursive_maze import RecursiveMaze
 
+graph = {}
 rows = 5
 cols = 3
 recursive_maze = RecursiveMaze(rows=rows, cols=cols)
@@ -18,18 +19,50 @@ maze_representation = [["  " for _ in range(cols * 2 + 1)] for _ in range(rows *
 for r in range(rows):
     for c in range(cols):
         cell = maze[r][c]
+        id = f"{r}-{c}"
+        tmpStr = ""     # "b,r,l,f"
+        tmpId = ""      # Cell ID to be added to tmpStr
+
         if cell.wall_back:
             maze_representation[r*2][c*2+1] = "# "
             maze_representation[r*2][c*2+2] = "# "
+            tmpId = ","
+        elif r-1>=0 and not maze[r-1][c].wall_front:
+            tmpId = f"{r-1}-{c},"
+        else:
+            tmpId = ","
+        tmpStr += tmpId
+        tmpId = ""
+
         if cell.wall_right:
             maze_representation[r*2+1][c*2+2] = "# "
             maze_representation[r*2+2][c*2+2] = "# "
+        elif c < cols-1 and not maze[r][c+1].wall_left:
+            tmpId = f"{r}-{c+1},"
+        else:
+            tmpId = ","
+        tmpStr += tmpId
+        tmpId = ""
+
         if cell.wall_left:
             maze_representation[r*2+1][c*2] = "# "
             maze_representation[r*2][c*2] = "# "
+            tmpId = ","
+        elif c-1>=0 and not maze[r][c-1].wall_right:
+            tmpId = f"{r}-{c-1},"
+        else:
+            tmpId = ","
+        tmpStr += tmpId
+        tmpId = ""
+        
         if cell.wall_front:
             maze_representation[r*2+2][c*2+1] = "# "
             maze_representation[r*2+2][c*2] = "# "
+        elif r+1<rows and not maze[r+1][c].wall_back:
+            tmpId = f"{r+1}-{c}"
+        tmpStr += tmpId
+
+        graph[id] = tmpStr
 
 # Set entrance and exit
 # Entrance is at top of maze
