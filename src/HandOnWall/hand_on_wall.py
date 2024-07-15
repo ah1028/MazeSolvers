b77f95122fa6483ecd84d77c0287f77e2504565b
Landydi0n <142226868+Landydi0n@users.noreply.github.com>
2024-07-15 16:17:06 +0100

    Latest changes

--- src/HandOnWall/hand_on_wall.py
+++ src/HandOnWall/hand_on_wall.py
@@ -2,8 +2,9 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from MazeGenerator.recursive_maze import RecursiveMaze
+import MazeGenerator.maze_spawner as ms
 
-recursive_maze = RecursiveMaze(rows=10, cols=10)
-recursive_maze.gen_maze()
-print(recursive_maze.maze[0][0].visited)
+# Graph is dictionary of cell Id and adjacent cells (b,r,l,f)
+# Also consider cell right is from looking face on but front is as if a player was entering the maze (l<->r)
+graph = ms.graph
+
